ae122bcc9d882ab2f948b29c54460be801a5c132
Tim Sjöstrand <tim.sjostrand@gmail.com>
2015-08-29 01:50:53 +0200

    separated color constants
    - c wont allow definitions in header without complaining about multiple definitions

--- color.h
+++ color.h
@@ -6,13 +6,13 @@
 #define rgb(v) xyz(v)
 #define rgba(v) xyzw(v)
 
-const vec4 COLOR_RED        = { 1.0f, 0.0f, 0.0f, 1.0f };
-const vec4 COLOR_GREEN      = { 0.0f, 1.0f, 0.0f, 1.0f };
-const vec4 COLOR_BLUE       = { 0.0f, 0.0f, 1.0f, 1.0f };
-const vec4 COLOR_WHITE      = { 1.0f, 1.0f, 1.0f, 1.0f };
-const vec4 COLOR_BLACK      = { 0.0f, 0.0f, 0.0f, 1.0f };
-const vec4 COLOR_YELLOW     = { 1.0f, 1.0f, 0.0f, 1.0f };
-const vec4 COLOR_CYAN       = { 0.0f, 1.0f, 1.0f, 1.0f };
-const vec4 COLOR_MAGENTA    = { 1.0f, 0.0f, 1.0f, 1.0f };
+const vec4 COLOR_RED;
+const vec4 COLOR_GREEN;
+const vec4 COLOR_BLUE;
+const vec4 COLOR_WHITE;
+const vec4 COLOR_BLACK;
+const vec4 COLOR_YELLOW;
+const vec4 COLOR_CYAN;
+const vec4 COLOR_MAGENTA;
 
 #endif
