ed938a8eeda2943a7f1f2efc16bee4ad5180e4da
realbogart <johan.yngman@gmail.com>
2015-09-06 17:33:19 +0200

    Some cleanup

--- vfs.h
+++ vfs.h
@@ -12,12 +12,11 @@ typedef void(*read_callback_t)(const char* filename, unsigned int size, void* da
 void	vfs_init();
 void	vfs_shutdown();
 void	vfs_mount(const char* dir);
-void	vfs_enable_filewatch();
 void	vfs_register_callback(const char* filename, read_callback_t fn);
 void	vfs_run_callbacks();
 void	vfs_filewatch();
 
 void*	vfs_get_file(const char* filename, unsigned int* out_num_bytes);
-void    vfs_free_memory(const char* filename);
+void	vfs_free_memory(const char* filename);
 
 #endif //_VFS_H
