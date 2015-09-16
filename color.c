86a67500fdf771ac5a6a6918f6dbc1810301f692
Tim Sjöstrand <tim.sjostrand@gmail.com>
2015-09-16 19:29:27 +0200

    color: fix trailing whitespace

--- color.c
+++ color.c
@@ -8,5 +8,3 @@ const vec4 COLOR_BLACK		= { 0.0f, 0.0f, 0.0f, 1.0f };
 const vec4 COLOR_YELLOW		= { 1.0f, 1.0f, 0.0f, 1.0f };
 const vec4 COLOR_CYAN		= { 0.0f, 1.0f, 1.0f, 1.0f };
 const vec4 COLOR_MAGENTA	= { 1.0f, 0.0f, 1.0f, 1.0f };
-
-
