4a934671bec2e8ebb55ce19c20eba7cb831363b8
Tim Sjöstrand <tim.sjostrand@gmail.com>
2016-03-27 23:34:55 +0200

    list.h: retabbed

--- list.h
+++ list.h
@@ -17,29 +17,29 @@ struct list;
  */
 struct element;
 
-struct list*    list_new();
-void            list_free(struct list *list, int free_data);
-int             list_prepend(struct list *list, void *data);
-int             list_append(struct list *list, void *data);
-int             list_empty(struct list *list);
-int             list_count(struct list *list);
-void            list_clear(struct list *list, int free_data);
+struct list*	list_new();
+void			list_free(struct list *list, int free_data);
+int				list_prepend(struct list *list, void *data);
+int				list_append(struct list *list, void *data);
+int				list_empty(struct list *list);
+int				list_count(struct list *list);
+void			list_clear(struct list *list, int free_data);
 struct element* list_head(struct list *list);
 struct element* list_first(struct list *list);
 struct element* list_last(struct list *list);
-int             list_insert_element_at(struct list *list, int index, struct element *e);
-int             list_insert_at(struct list *list, int index, void *data);
-int             list_index_of(struct list *list, void *data);
+int				list_insert_element_at(struct list *list, int index, struct element *e);
+int				list_insert_at(struct list *list, int index, void *data);
+int				list_index_of(struct list *list, void *data);
 struct element* list_get_element(struct list *list, void *data);
 struct element* list_element_at(struct list *list, int index);
-void            list_element_init(struct element *e, void *data);
+void			list_element_init(struct element *e, void *data);
 struct element* list_element_new(void *data);
-void            list_element_free(struct element *e, int free_data);
-void            list_element_delete(struct element *e, int free_data);
-int             list_init(struct list *list);
-void            list_prepend_element(struct list *list, struct element *e);
-void            list_append_element(struct list *list, struct element *e);
-void            list_print_strings(struct list *list);
+void			list_element_free(struct element *e, int free_data);
+void			list_element_delete(struct element *e, int free_data);
+int				list_init(struct list *list);
+void			list_prepend_element(struct list *list, struct element *e);
+void			list_append_element(struct list *list, struct element *e);
+void			list_print_strings(struct list *list);
 
 struct list*	list_copy(struct list *original);
 struct list*	list_copy_subset(struct list *original, size_t start, size_t count);
@@ -50,18 +50,18 @@ struct list*	list_copy_subset(struct list *original, size_t start, size_t count)
  *
  * @code
  * foreach_list(struct str_element*, e, list) {
- *     printf("%d=%s\n", index, e->data);
+ *	   printf("%d=%s\n", index, e->data);
  * }
  * @endcode
  *
- * @param type  The pointer type of elements in the list.
- * @param item  The name assigned to the currently iterated element pointer.
- * @param list  The list to iterate over.
+ * @param type	The pointer type of elements in the list.
+ * @param item	The name assigned to the currently iterated element pointer.
+ * @param list	The list to iterate over.
  */
 #define foreach_list(type, item, list) \
 	for(type item = (type) list_first(list); \
-                (item != NULL) && ((struct element*) item != list_head(list)); \
-                item = (type) item->next)
+				(item != NULL) && ((struct element*) item != list_head(list)); \
+				item = (type) item->next)
 
 /**
  * Iterates a list in reverse.
@@ -70,8 +70,8 @@ struct list*	list_copy_subset(struct list *original, size_t start, size_t count)
  */
 #define foreach_reverse(type, item, list) \
 	for(type item = (type) list_last(list); \
-                (item != NULL) && ((struct element*) item != list_head(list)); \
-                item = (type) item->prev)
+				(item != NULL) && ((struct element*) item != list_head(list)); \
+				item = (type) item->prev)
 
 /**
  * Defines a new list element where 'data' is a pointer of the specified type.
@@ -86,11 +86,11 @@ struct list*	list_copy_subset(struct list *original, size_t start, size_t count)
  * };
  * @endcode
  *
- * @param name  The struct declaration including the name for the element.
- * @param type  The data pointer type.
+ * @param name	The struct declaration including the name for the element.
+ * @param type	The data pointer type.
  */
 #define define_element(name, type) \
-        name { \
+	name { \
 		type data; \
 		struct element *next; \
 		struct element *prev; \
