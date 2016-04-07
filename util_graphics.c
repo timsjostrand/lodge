dddebe74b7286e11e434cfe5d442101b3ba94bfe
Tim Sjöstrand <tim.sjostrand@gmail.com>
2016-04-07 09:16:40 +0200

    util_graphics: updated documentation

--- util_graphics.c
+++ util_graphics.c
@@ -1,5 +1,7 @@
 /**
- * Important stuff
+ * Graphics helpers.
+ *
+ * Author: Tim Sjöstrand <tim.sjostrand@gmail.com>
  */
 
 #include <GLFW/glfw3.h>
@@ -7,19 +9,46 @@
 #include "util_graphics.h"
 #include "math4.h"
 
+/**
+ * Retrieve the OpenGL viewport.
+ *
+ * @param x	Viewport X destination (or NULL to skip).
+ * @param y Viewport Y destination (or NULL to skip).
+ * @param w Viewport width destination (or NULL to skip).
+ * @param h Viewport height destination (or NULL to skip).
+ */
 void util_get_viewport(float* x, float* y, float* w, float* h)
 {
 	float buffer[4];
 	glGetFloatv(GL_VIEWPORT, buffer);
 
-	*x = buffer[0];
-	*y = buffer[1];
-	*w = buffer[2];
-	*h = buffer[3];
+	if(x != NULL) {
+		*x = buffer[0];
+	}
+
+	if(y != NULL) {
+		*y = buffer[1];
+	}
+
+	if(w != NULL) {
+		*w = buffer[2];
+	}
+
+	if(h != NULL) {
+		*h = buffer[3];
+	}
 }
 
 /**
  * Converts window corrdinates to view coordinates.
+ *
+ * @param win_x			X coordinate in window space.
+ * @param win_y			Y coordinate in window space.
+ * @param win_w			Window width.
+ * @param win_h			Window height.
+ * @param x				Destination X in view space.
+ * @param y				Destination Y in view space.
+ * @param projection	View projection matrix.
  */
 void util_window_to_view(float win_x, float win_y, float win_w, float win_h, float *x, float *y, mat4 projection)
 {
@@ -49,6 +78,13 @@ void util_window_to_view(float win_x, float win_y, float win_w, float win_h, flo
 	*y = out_pos[1];
 }
 
+/**
+ * Retrieve the cursor position in window space.
+ *
+ * @param window	GLFW window handle.
+ * @param x			X coordinate destination.
+ * @param y			Y coordinate destination.
+ */
 void util_window_get_cursor(GLFWwindow *window, float *x, float *y)
 {
 	double tmp_x = 0, tmp_y = 0;
@@ -57,6 +93,14 @@ void util_window_get_cursor(GLFWwindow *window, float *x, float *y)
 	*y = (float) tmp_y;
 }
 
+/**
+ * Retrieve cursor position in view space.
+ *
+ * @param window		GLFW window handle.
+ * @param x				X coordinate destination.
+ * @param y				Y coordinate destination.
+ * @param projection	View projection matrix.
+ */
 void util_view_get_cursor(GLFWwindow *window, float *x, float *y, mat4 projection)
 {
 	float win_x = 0, win_y = 0;
