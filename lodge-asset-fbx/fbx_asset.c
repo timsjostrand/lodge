112b84313173201ee24c2672e7bfb38cc156c5a0
Tim Sjöstrand <tim.sjostrand@gmail.com>
2019-05-15 13:20:10 +0200

    lodge-asset-fbx: wip

--- lodge-asset-fbx/fbx_asset.c
+++ lodge-asset-fbx/fbx_asset.c
@@ -1,38 +1,21 @@
 #include "fbx_asset.h"
 
-#include <stdint.h>
 #include <GL/glew.h>
 
-#include "math4.h"
 #include "lodge_platform.h"
+#include "math4.h"
 #include "graphics.h"
 #include "fbx.h"
 #include "str.h"
 
-static struct fbx_property* fbx_asset_get_array_double(struct fbx *fbx, const char *path[], size_t path_count)
+static struct fbx_property* fbx_asset_get_array(struct fbx *fbx, const char *path[], size_t path_count)
 {
 	struct fbx_node *node = fbx_get_node(fbx, path, path_count);
 	if(node) {
 		const uint32_t property_count = fbx_node_get_property_count(node);
 		for(uint32_t i = 0; i < property_count; i++) {
-			struct fbx_property *prop = fbx_node_get_property(node, i);
-			if(fbx_property_get_type(prop) == FBX_PROPERTY_TYPE_ARRAY_DOUBLE) {
-				return prop;
-			}
-		}
-	}
-	ASSERT_FAIL("fbx_asset: failed to find array property");
-	return NULL;
-}
-
-static struct fbx_property* fbx_asset_get_array_int32(struct fbx *fbx, const char *path[], size_t path_count)
-{
-	struct fbx_node *node = fbx_get_node(fbx, path, path_count);
-	if(node) {
-		const uint32_t property_count = fbx_node_get_property_count(node);
-		for(uint32_t i = 0; i < property_count; i++) {
-			struct fbx_property *prop = fbx_node_get_property(node, i);
-			if(fbx_property_get_type(prop) == FBX_PROPERTY_TYPE_ARRAY_INT32) {
+			struct fbx_property *prop = fbx_node_get_property_array(node, i);
+			if(prop) {
 				return prop;
 			}
 		}
@@ -58,6 +41,108 @@ static struct fbx_string fbx_asset_get_string(struct fbx *fbx, const char *path[
 	return empty;
 }
 
+struct float_array
+{
+	float*	data;
+	size_t	size;
+};
+
+void float_array_reset(struct float_array *a)
+{
+	free(a->data);
+	a->data = 0;
+	a->size = 0;
+}
+
+static struct float_array fbx_asset_new_layer_element(
+	struct fbx *fbx,
+	const int32_t vertex_indices[], size_t vertex_indices_count,
+	const char *mapping_type_path[], size_t mapping_type_path_count,
+	const char *ref_type_path[], size_t ref_type_path_count,
+	const char *data_path[], size_t data_path_count,
+	const char *indices_path[], size_t indices_path_count
+)
+{
+	struct float_array ret = { 0 };
+
+	/* Check mapping type */
+	{
+		struct fbx_string mapping_type_str = fbx_asset_get_string(fbx, mapping_type_path, mapping_type_path_count);
+		strview_t mapping_type = strview_make(mapping_type_str.data, mapping_type_str.length);
+
+		if(!strview_equals(mapping_type, strview_static("ByPolygonVertex"))) {
+			ASSERT_FAIL("Layer element mapping != `ByPolygonVertex` not implemented");
+			goto fail;
+		}
+	}
+
+	/* Check reference type */
+	enum ref_type {
+		direct,
+		index_to_direct
+	} ref_type;
+	{
+		struct fbx_string ref_type_prop = fbx_asset_get_string(fbx, ref_type_path, ref_type_path_count);
+		strview_t ref_type_str = strview_make(ref_type_prop.data, ref_type_prop.length);
+		if(strview_equals(ref_type_str, strview_static("Direct"))) {
+			ref_type = direct;
+		} else if(strview_equals(ref_type_str, strview_static("IndexToDirect"))) {
+			ref_type = index_to_direct;
+		} else {
+			ASSERT_FAIL("Reference type not implemented");
+			goto fail;
+		}
+	}
+
+	struct fbx_property *prop_data = fbx_asset_get_array(fbx, data_path, data_path_count);
+	const double* prop_data_ptr = fbx_property_get_array_double(prop_data);
+	const uint32_t prop_data_count = fbx_property_get_array_count(prop_data);
+	if(!prop_data || !prop_data_ptr || !prop_data_count) {
+		ASSERT_FAIL("Failed to find layer element data property");
+		goto fail;
+	}
+
+	struct fbx_property *prop_indices = fbx_asset_get_array(fbx, indices_path, indices_path_count);
+	const int32_t* prop_indices_ptr = fbx_property_get_array_int32(prop_indices);
+	const uint32_t prop_indices_count = fbx_property_get_array_count(prop_indices);
+	if(!prop_indices || !prop_indices_ptr || !prop_indices_count) {
+		ASSERT_FAIL("Failed to find layer element index property");
+		goto fail;
+	}
+
+	if(prop_indices_count != vertex_indices_count) {
+		ASSERT_FAIL("Incorrect number of indices");
+		goto fail;
+	}
+
+	ret.data = (float*)malloc(sizeof(float) * prop_indices_count);
+	ret.size = prop_indices_count;
+
+	int32_t prop_index_max = 0;
+	for(uint32_t i = 0; i < prop_indices_count; i++) {
+		if(ref_type == direct) {
+			ret.data[i] = (float)prop_data_ptr[i];
+		} else if(ref_type == index_to_direct) {
+			const int32_t prop_index = prop_indices_ptr[i];
+			ASSERT(prop_index >= 0);
+			ASSERT((uint32_t)prop_index < prop_indices_count);
+			ASSERT(i < vertex_indices_count);
+			const int32_t vertex_index = vertex_indices[i];
+			ret.data[vertex_index] = (float)prop_data_ptr[prop_index];
+			prop_index_max = max(prop_index, prop_index_max);
+		} else {
+			ASSERT_FAIL("Reference type not implemented");
+			goto fail;
+		}
+	}
+
+	return ret;
+
+fail:
+	float_array_reset(&ret);
+	return ret;
+}
+
 struct fbx_asset fbx_asset_make(struct fbx *fbx)
 {
 	struct fbx_asset asset = { 0 };
@@ -78,7 +163,7 @@ struct fbx_asset fbx_asset_make(struct fbx *fbx)
 
 		static const char* path_vertices[] = { "Objects", "Geometry", "Vertices" };
 
-		struct fbx_property *vertices_prop = fbx_asset_get_array_double(fbx, path_vertices, LODGE_ARRAYSIZE(path_vertices));
+		struct fbx_property *vertices_prop = fbx_asset_get_array(fbx, path_vertices, LODGE_ARRAYSIZE(path_vertices));
 		if(!vertices_prop) {
 			goto fail;
 		}
@@ -127,7 +212,7 @@ struct fbx_asset fbx_asset_make(struct fbx *fbx)
 			glBindBuffer(GL_ARRAY_BUFFER, asset.buffer_object_normals);
 			GL_OK_OR_GOTO(fail);
 
-			struct fbx_property *normals_prop = fbx_asset_get_array_double(fbx, path_normals, LODGE_ARRAYSIZE(path_normals));
+			struct fbx_property *normals_prop = fbx_asset_get_array(fbx, path_normals, LODGE_ARRAYSIZE(path_normals));
 			if(!normals_prop) {
 				goto fail;
 			}
@@ -152,49 +237,100 @@ struct fbx_asset fbx_asset_make(struct fbx *fbx)
 		}
 	}
 
-	/* Indices */
-	{
-		glGenBuffers(1, &asset.buffer_object_indices);
-		GL_OK_OR_GOTO(fail);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, asset.buffer_object_indices);
-		GL_OK_OR_GOTO(fail);
-
-		static const char* path_indices[] = { "Objects", "Geometry", "PolygonVertexIndex" };
-
-		struct fbx_property *indices_prop = fbx_asset_get_array_int32(fbx, path_indices, LODGE_ARRAYSIZE(path_indices));
-		if(!indices_prop) {
-			goto fail;
-		}
-
-		const int32_t* indices_prop_data = fbx_property_get_array_int32(indices_prop);
-		const uint32_t indices_prop_data_count = fbx_property_get_array_count(indices_prop);
-
-		if(indices_prop_data_count < 3) {
-			ASSERT_FAIL("Very small index buffer");
-			goto fail;
-		}
-
-		int triangle_format = indices_prop_data[2] < 0;
-		if(!triangle_format) {
-			ASSERT_FAIL("Non triangle vertex format is not supported");
-			goto fail;
-		}
-
-		/* Convert signed FBX indices to unsigned indices */
-		uint32_t* indices = malloc(indices_prop_data_count * sizeof(uint32_t));
-		for(uint32_t i = 0; i < indices_prop_data_count; i++) {
-			const int32_t index = indices_prop_data[i];
-			indices[i] = index >= 0 ? (uint32_t)index : (uint32_t)(~index);
-		}
-
-		asset.indices_count = indices_prop_data_count;
-
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_prop_data_count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
-		GL_OK_OR_GOTO(fail);
-
-		free(indices);
+	static const char* path_indices[] = { "Objects", "Geometry", "PolygonVertexIndex" };
+	struct fbx_property *prop_indices = fbx_asset_get_array(fbx, path_indices, LODGE_ARRAYSIZE(path_indices));
+	if(!prop_indices) {
+		goto fail;
 	}
+
+	const int32_t* prop_indices_data = fbx_property_get_array_int32(prop_indices);
+	const uint32_t prop_indices_data_count = fbx_property_get_array_count(prop_indices);
+
+	if(prop_indices_data_count < 3) {
+		ASSERT_FAIL("Vertex index buffer too small");
+		goto fail;
+	}
+
+	enum polygon_type {
+		triangle,
+		quad
+	} polygon_type;
+
+	if(prop_indices_data[2] < 0) {
+		polygon_type = triangle;
+	} else if(prop_indices_data[3] < 0) {
+		polygon_type = quad;
+	} else {
+		ASSERT_FAIL("Unknown polygon type");
+		goto fail;
+	}
+
+	if(polygon_type != triangle) {
+		ASSERT_FAIL("polygon_type != triangle not implemented");
+		goto fail;
+	}
+
+	/* Convert signed FBX indices to unsigned indices */
+	uint32_t* indices = malloc(prop_indices_data_count * sizeof(uint32_t));
+	for(uint32_t i = 0; i < prop_indices_data_count; i++) {
+		const int32_t index = prop_indices_data[i];
+		indices[i] = index >= 0 ? (uint32_t)index : (uint32_t)(~index);
+	}
+
+	asset.indices_count = prop_indices_data_count;
+
+	/* UVs */
+	{
+		static const char* path_uv_mapping_type[] = { "Objects", "Geometry", "LayerElementUV", "MappingInformationType" };
+		static const char* path_uvs_ref_type[] = { "Objects", "Geometry", "LayerElementUV", "ReferenceInformationType" };
+		static const char* path_uvs_data[] = { "Objects", "Geometry", "LayerElementUV", "UV" };
+		static const char* path_uvs_indices[] = { "Objects", "Geometry", "LayerElementUV", "UVIndex" };
+
+		struct float_array uvs = fbx_asset_new_layer_element(fbx,
+			indices,				asset.indices_count,
+			path_uv_mapping_type,	LODGE_ARRAYSIZE(path_uv_mapping_type),
+			path_uvs_ref_type,		LODGE_ARRAYSIZE(path_uvs_ref_type),
+			path_uvs_data,			LODGE_ARRAYSIZE(path_uvs_data),
+			path_uvs_indices,		LODGE_ARRAYSIZE(path_uvs_indices)
+		);
+
+		if(!uvs.data || !uvs.size) {
+			goto fail;
+		}
+
+		glGenBuffers(1, &asset.buffer_object_uvs);
+		GL_OK_OR_GOTO(fail);
+
+		glBindBuffer(GL_ARRAY_BUFFER, asset.buffer_object_uvs);
+		GL_OK_OR_GOTO(fail);
+
+		glBufferData(GL_ARRAY_BUFFER, uvs.size * sizeof(float), uvs.data, GL_STATIC_DRAW);
+		free(uvs.data);
+		GL_OK_OR_GOTO(fail);
+
+		glEnableVertexAttribArray(2);
+		GL_OK_OR_GOTO(fail);
+
+		glVertexAttribPointer(2,
+			2,
+			GL_FLOAT,
+			GL_FALSE,
+			2 * sizeof(float),
+			NULL
+		);
+		GL_OK_OR_GOTO(fail);
+	}
+
+	/* Indices */
+	glGenBuffers(1, &asset.buffer_object_indices);
+	GL_OK_OR_GOTO(fail);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, asset.buffer_object_indices);
+	GL_OK_OR_GOTO(fail);
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, prop_indices_data_count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+	GL_OK_OR_GOTO(fail);
+
 
 	/* Reset bindings to default */
 	glBindVertexArray(0);
@@ -203,6 +339,7 @@ struct fbx_asset fbx_asset_make(struct fbx *fbx)
 	return asset;
 
 fail:
+	free(indices);
 	fbx_asset_reset(&asset);
 	return asset;
 }
@@ -211,42 +348,55 @@ void fbx_asset_reset(struct fbx_asset *asset)
 {
 	if(asset->buffer_object_indices) {
 		glDeleteBuffers(1, &asset->buffer_object_indices);
+		GL_OK_OR_ASSERT("Failed to delete indices buffer object");
 	}
 	if(asset->buffer_object_vertices) {
 		glDeleteBuffers(1, &asset->buffer_object_vertices);
+		GL_OK_OR_ASSERT("Failed to delete vertices buffer object");
 	}
 	if(asset->buffer_object_normals) {
 		glDeleteBuffers(1, &asset->buffer_object_normals);
+		GL_OK_OR_ASSERT("Failed to delete normals buffer object");
 	}
 	if(asset->vertex_array_object) {
 		glDeleteVertexArrays(1, &asset->vertex_array_object);
+		GL_OK_OR_ASSERT("Failed to delete vertex array");
 	}
 
 	struct fbx_asset tmp = { 0 };
 	*asset = tmp;
 }
 
-void fbx_asset_render(struct fbx_asset *asset, struct shader *shader, struct mvp mvp)
+void fbx_asset_render(struct fbx_asset *asset, struct shader *shader, int tex, struct mvp mvp)
 {
 	glUseProgram(shader->program);
 	GL_OK_OR_ASSERT("Failed to use shader program");
 
+	/* TODO(TS): Upload MVP as struct instead */
 	{
-		GLint uniform_projection = glGetUniformLocation(shader->program, "projection");
-		glUniformMatrix4fv(uniform_projection, 1, GL_FALSE, (const GLfloat*)mvp.projection.m);
-		GL_OK_OR_ASSERT("Failed to set projection");
+		{
+			GLint uniform_projection = glGetUniformLocation(shader->program, "projection");
+			glUniformMatrix4fv(uniform_projection, 1, GL_FALSE, (const GLfloat*)mvp.projection.m);
+			GL_OK_OR_ASSERT("Failed to set projection");
+		}
+
+		{
+			GLint uniform_view = glGetUniformLocation(shader->program, "view");
+			glUniformMatrix4fv(uniform_view, 1, GL_FALSE, (const GLfloat*)mvp.view.m);
+			GL_OK_OR_ASSERT("Failed to set view");
+		}
+
+		{
+			GLint uniform_model = glGetUniformLocation(shader->program, "model");
+			glUniformMatrix4fv(uniform_model, 1, GL_FALSE, (const GLfloat*)mvp.model.m);
+			GL_OK_OR_ASSERT("Failed to set model");
+		}
 	}
 
+	// FIXME(TS): material should not be hardcoded
 	{
-		GLint uniform_view = glGetUniformLocation(shader->program, "view");
-		glUniformMatrix4fv(uniform_view, 1, GL_FALSE, (const GLfloat*)mvp.view.m);
-		GL_OK_OR_ASSERT("Failed to set view");
-	}
-	
-	{
-		GLint uniform_model = glGetUniformLocation(shader->program, "model");
-		glUniformMatrix4fv(uniform_model, 1, GL_FALSE, (const GLfloat*)mvp.model.m);
-		GL_OK_OR_ASSERT("Failed to set model");
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, tex);
 	}
 
 	glBindVertexArray(asset->vertex_array_object);
