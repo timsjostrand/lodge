7c469536c0885f554b75338805fe14972bdaadac
Tim Sjöstrand <tim.sjostrand@gmail.com>
2019-09-18 13:51:35 +0200

    lodge_platform: include standard numeric typedefs

--- lodge_platform.h
+++ lodge_platform.h
@@ -1,6 +1,9 @@
 #ifndef _LODGE_PLATFORM_H
 #define _LODGE_PLATFORM_H
 
+#include <stddef.h> // size_t
+#include <stdint.h>
+
 #include "lodge_assert.h"
 
 #define LODGE_ARRAYSIZE(a) ( sizeof(a) / sizeof(a[0]) )
