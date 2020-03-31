aea5f298e5df1c405a9aed3e49566f9bf62db90c
Tim Sjöstrand <tim.sjostrand@gmail.com>
2020-04-01 01:09:49 +0200

    lodge-renderer: clean up

--- lodge-renderer/lodge_texture_opengl.c
+++ lodge-renderer/lodge_texture_opengl.c
@@ -1,8 +1,9 @@
 #include "lodge_texture.h"
 
+#include "lodge_image.h"
 #include "lodge_opengl.h"
 
-static lodge_texture_t lodge_texture_make(int width, int height, GLint internal_format, GLint format, GLenum type)
+static lodge_texture_t lodge_texture_make_details(int width, int height, GLint internal_format, GLint format, GLenum type)
 {
 	GLuint name;
 
@@ -18,15 +19,77 @@ static lodge_texture_t lodge_texture_make(int width, int height, GLint internal_
 	return lodge_texture_from_gl(name);
 }
 
+struct gl_pixel_format
+{
+	int internal_format;
+	int pixel_format;
+};
+
+static struct gl_pixel_format lodge_image_to_gl_pixel_format(const struct lodge_image *image)
+{
+	switch(image->components)
+	{
+	case 1:
+		return (struct gl_pixel_format) {
+			.internal_format = GL_R8,
+			.pixel_format = GL_RED,
+		};
+	case 3:
+		return (struct gl_pixel_format) {
+			.internal_format = GL_RGB,
+			.pixel_format = GL_RGB,
+		};
+	case 4:
+		return (struct gl_pixel_format) {
+			.internal_format = GL_RGBA,
+			.pixel_format = GL_RGBA,
+		};
+	default:
+		ASSERT_FAIL("Unsupported pixel format");
+		return (struct gl_pixel_format) { 0 };
+	}
+}
+
+static lodge_texture_t lodge_texture_make_from_pixels(const uint8_t *data, struct gl_pixel_format format, int width, int height)
+{
+	GLuint gl_tex;
+	glGenTextures(1, &gl_tex);
+	GL_OK_OR_RETURN(0);
+
+	glBindTexture(GL_TEXTURE_2D, gl_tex);
+	GL_OK_OR_RETURN(0);
+
+	/* Upload texture. */
+	glTexImage2D(GL_TEXTURE_2D, 0, format.internal_format, width, height, 0, format.pixel_format, GL_UNSIGNED_BYTE, data);
+	GL_OK_OR_RETURN(0);
+
+	return lodge_texture_from_gl(gl_tex);
+}
+
+lodge_texture_t lodge_texture_make()
+{
+	GLuint name;
+	glGenTextures(1, &name);
+	GL_OK_OR_ASSERT("glGenTextures failed");
+	return lodge_texture_from_gl(name);
+}
+
 lodge_texture_t lodge_texture_make_rgba(int width, int height)
 {
-	return lodge_texture_make(width, height, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE);
+	return lodge_texture_make_details(width, height, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE);
 }
 
 lodge_texture_t lodge_texture_make_depth(int width, int height)
 {
-	return lodge_texture_make(width, height, GL_DEPTH_COMPONENT32, GL_DEPTH_COMPONENT, GL_FLOAT);
+	return lodge_texture_make_details(width, height, GL_DEPTH_COMPONENT32, GL_DEPTH_COMPONENT, GL_FLOAT);
 }
+
+lodge_texture_t lodge_texture_make_from_image(const struct lodge_image *image)
+{
+	struct gl_pixel_format format = lodge_image_to_gl_pixel_format(image);
+	return lodge_texture_make_from_pixels(image->pixel_data, format, image->width, image->height);
+}
+
 
 void lodge_texture_reset(lodge_texture_t *texture)
 {
@@ -35,5 +98,10 @@ void lodge_texture_reset(lodge_texture_t *texture)
 	ASSERT(name > 0);
 	glDeleteTextures(1, &name);
 	GL_OK_OR_ASSERT("Failed to delete texture");
-	texture = 0;
+	texture = NULL;
+}
+
+int lodge_texture_is_valid(lodge_texture_t texture)
+{
+	return lodge_texture_to_gl(texture) != 0;
 }
