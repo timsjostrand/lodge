3156ab7cb1b58010e8265d9d50c9715c91b5a1da
Tim Sjöstrand <tim.sjostrand@gmail.com>
2020-04-01 23:46:35 +0200

    removed unused code

--- lodge_interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LODGE_INTERFACE_H
-#define _LODGE_INTERFACE_H
-
-#include "lodge.h"
-
-EXPORT void lodgei_start(struct lodge_settings* settings, const char* mount_path, int window_mode);
-
-#endif
