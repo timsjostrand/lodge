ceff27dd5d121e8411558db36f80d9eda7d785e0
Tim Sjöstrand <tim.sjostrand@gmail.com>
2020-04-09 01:55:47 +0200

    lodge-console : move from core to module

--- lodge-console/core_console.h
+++ lodge-console/core_console.h
@@ -1,12 +1,12 @@
 #ifndef _CORE_CONSOLE_H
 #define _CORE_CONSOLE_H
 
+#if 0
 struct console;
 struct env;
 
 void	core_console_new(struct console *c, struct env *env);
 void	core_console_printf(const char *fmt, ...);
-#if 0
 void	core_console_free(struct core_console *cmds);
 #endif
 
