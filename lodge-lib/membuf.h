c892c1ed9cf3252a0672928a8b38e86eb3de9cd3
Tim Sjöstrand <tim.sjostrand@gmail.com>
2020-08-25 09:00:57 +0200

    lodge-lib: membuf: api fixes

--- lodge-lib/membuf.h
+++ lodge-lib/membuf.h
@@ -22,26 +22,32 @@ typedef struct membuf
 	char*		ptr;			// ptr to array
 } membuf_t;
 
+struct membuf_swapret
+{
+	size_t		index_a;
+	size_t		index_b;
+};
+
 // typedef const membuf_t memview_t;
 
-membuf_t	membuf_make(char *ptr, size_t size, size_t type_size);
-#define		membuf_wrap(buffer) membuf_make((char*)(buffer), sizeof((buffer)), sizeof(buffer[0]))
-#define		membuf_wrap_and(arr, func, ...) func(membuf_wrap(arr), __VA_ARGS__)
+membuf_t				membuf_make(char *ptr, size_t size, size_t type_size);
+#define					membuf_wrap(buffer) membuf_make((char*)(buffer), sizeof((buffer)), sizeof(buffer[0]))
+#define					membuf_wrap_and(arr, func, ...) func(membuf_wrap(arr), __VA_ARGS__)
 
-size_t		membuf_size(const membuf_t buf);
-size_t		membuf_type_size(const membuf_t buf);
-size_t		membuf_max_count(const membuf_t buf);
+size_t					membuf_size(const membuf_t buf);
+size_t					membuf_type_size(const membuf_t buf);
+size_t					membuf_max_count(const membuf_t buf);
 
-void*		membuf_get(membuf_t buf, size_t index);
-void		membuf_set(membuf_t buf, size_t index, const void *src, size_t src_size);
+void*					membuf_get(membuf_t buf, size_t index);
+void					membuf_set(membuf_t buf, size_t index, const void *src, size_t src_size);
 
-bool		membuf_equals(const membuf_t lhs, const membuf_t rhs);
-//size_t	membuf_insert(membuf_t buf, size_t index, const membuf_t sub);
-//size_t	membuf_delete(membuf_t buf, size_t index, size_t count, size_t *current_count);
-void		membuf_delete_swap_tail(membuf_t buf, size_t index, size_t *current_count);
-void		membuf_swap(membuf_t buf, size_t index_a, size_t index_b);
-void		membuf_append(membuf_t dst, void *src, size_t src_size, size_t *current_count);
-void		membuf_fill(membuf_t dst, void *src, size_t src_size);
+bool					membuf_equals(const membuf_t lhs, const membuf_t rhs);
+//size_t				membuf_insert(membuf_t buf, size_t index, const membuf_t sub);
+//size_t				membuf_delete(membuf_t buf, size_t index, size_t count, size_t *current_count);
+struct membuf_swapret	membuf_delete_swap_tail(membuf_t buf, size_t index, size_t *current_count);
+void					membuf_swap(membuf_t buf, size_t index_a, size_t index_b);
+void					membuf_append(membuf_t dst, const void *src, size_t src_size, size_t *current_count);
+void					membuf_fill(membuf_t dst, const void *src, size_t src_size);
 
 //
 // Returns the maximum index of the membuf (ie. max_count - 1).
