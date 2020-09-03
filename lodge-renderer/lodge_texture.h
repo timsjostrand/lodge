dccef7b3c7d62668e5c524344b3a3efc4c78194e
Tim Sjöstrand <tim.sjostrand@gmail.com>
2020-09-03 14:03:27 +0200

    lodge-renderer: adds cubemap textures

--- lodge-renderer/lodge_texture.h
+++ lodge-renderer/lodge_texture.h
@@ -12,12 +12,23 @@ enum lodge_texture_target
 struct lodge_texture;
 typedef struct lodge_texture* lodge_texture_t;
 
+struct lodge_texture_cubemap_desc
+{
+	const struct lodge_image *front;
+	const struct lodge_image *back;
+	const struct lodge_image *top;
+	const struct lodge_image *bottom;
+	const struct lodge_image *left;
+	const struct lodge_image *right;
+};
+
 struct lodge_image;
 
 lodge_texture_t		lodge_texture_make();
 lodge_texture_t		lodge_texture_make_rgba(uint32_t width, uint32_t height);
 lodge_texture_t		lodge_texture_make_depth(uint32_t width, uint32_t height);
 lodge_texture_t		lodge_texture_make_from_image(const struct lodge_image *image);
+lodge_texture_t		lodge_texture_make_cubemap(struct lodge_texture_cubemap_desc desc);
 
 void				lodge_texture_reset(lodge_texture_t *texture);
 
