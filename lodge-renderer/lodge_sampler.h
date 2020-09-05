c1156468e743ab0bd3e04d10f94cc9c41a359b73
Tim Sjöstrand <tim.sjostrand@gmail.com>
2020-09-06 00:23:31 +0200

    remove unused code

--- lodge-renderer/lodge_sampler.h
+++ lodge-renderer/lodge_sampler.h
@@ -37,17 +37,17 @@ struct lodge_sampler_properties
 	enum lodge_sampler_wrap			wrap_z;
 };
 
-lodge_sampler_t		lodge_sampler_make();
-lodge_sampler_t		lodge_sampler_make_properties(struct lodge_sampler_properties properties);
-void				lodge_sampler_reset(lodge_sampler_t *sampler);
+lodge_sampler_t						lodge_sampler_make();
+lodge_sampler_t						lodge_sampler_make_properties(struct lodge_sampler_properties properties);
+void								lodge_sampler_reset(lodge_sampler_t *sampler);
 
-void				lodge_sampler_set_min_filter(lodge_sampler_t sampler, enum lodge_sampler_min_filter min_filter);
-void				lodge_sampler_set_mag_filter(lodge_sampler_t sampler, enum lodge_sampler_mag_filter mag_filter);
+void								lodge_sampler_set_min_filter(lodge_sampler_t sampler, enum lodge_sampler_min_filter min_filter);
+void								lodge_sampler_set_mag_filter(lodge_sampler_t sampler, enum lodge_sampler_mag_filter mag_filter);
 
-void				lodge_sampler_set_wrap_x(lodge_sampler_t sampler, enum lodge_sampler_wrap wrap);
-void				lodge_sampler_set_wrap_y(lodge_sampler_t sampler, enum lodge_sampler_wrap wrap);
-void				lodge_sampler_set_wrap_z(lodge_sampler_t sampler, enum lodge_sampler_wrap wrap);
+void								lodge_sampler_set_wrap_x(lodge_sampler_t sampler, enum lodge_sampler_wrap wrap);
+void								lodge_sampler_set_wrap_y(lodge_sampler_t sampler, enum lodge_sampler_wrap wrap);
+void								lodge_sampler_set_wrap_z(lodge_sampler_t sampler, enum lodge_sampler_wrap wrap);
 
-void				lodge_sampler_set_properties(lodge_sampler_t sampler, struct lodge_sampler_properties properties);
+void								lodge_sampler_set_properties(lodge_sampler_t sampler, struct lodge_sampler_properties properties);
 
 #endif
