ef6a66791f07a5f4ebb6b9c59dbc71d948a59d10
Tim Sjöstrand <tim.sjostrand@gmail.com>
2020-12-14 14:09:23 +0100

    lodge-lib: adds gruvbox.h

---
+++ lodge-lib/gruvbox.c
@@ -0,0 +1,27 @@
+#include "gruvbox.h"
+
+#include "color.h"
+
+const vec4 GRUVBOX_BRIGHT_RED			= HEX_TO_RGBA( 0xfb4934, 1.0f );
+const vec4 GRUVBOX_BRIGHT_GREEN			= HEX_TO_RGBA( 0xb8bb26, 1.0f );
+const vec4 GRUVBOX_BRIGHT_YELLOW		= HEX_TO_RGBA( 0xfabd2f, 1.0f );
+const vec4 GRUVBOX_BRIGHT_BLUE			= HEX_TO_RGBA( 0x83a598, 1.0f );
+const vec4 GRUVBOX_BRIGHT_PURPLE		= HEX_TO_RGBA( 0xd3869b, 1.0f );
+const vec4 GRUVBOX_BRIGHT_AQUA			= HEX_TO_RGBA( 0x8ec07c, 1.0f );
+const vec4 GRUVBOX_BRIGHT_ORANGE		= HEX_TO_RGBA( 0xfe8019, 1.0f );
+
+const vec4 GRUVBOX_NEUTRAL_RED			= HEX_TO_RGBA( 0xcc241d, 1.0f );
+const vec4 GRUVBOX_NEUTRAL_GREEN		= HEX_TO_RGBA( 0x98971a, 1.0f );
+const vec4 GRUVBOX_NEUTRAL_YELLOW		= HEX_TO_RGBA( 0xd79921, 1.0f );
+const vec4 GRUVBOX_NEUTRAL_BLUE			= HEX_TO_RGBA( 0x458588, 1.0f );
+const vec4 GRUVBOX_NEUTRAL_PURPLE		= HEX_TO_RGBA( 0xb16286, 1.0f );
+const vec4 GRUVBOX_NEUTRAL_AQUA			= HEX_TO_RGBA( 0x689d6a, 1.0f );
+const vec4 GRUVBOX_NEUTRAL_ORANGE		= HEX_TO_RGBA( 0xd65d0e, 1.0f );
+
+const vec4 GRUVBOX_FADED_RED			= HEX_TO_RGBA( 0x9d0006, 1.0f );
+const vec4 GRUVBOX_FADED_GREEN			= HEX_TO_RGBA( 0x79740e, 1.0f );
+const vec4 GRUVBOX_FADED_YELLOW			= HEX_TO_RGBA( 0xb57614, 1.0f );
+const vec4 GRUVBOX_FADED_BLUE			= HEX_TO_RGBA( 0x076678, 1.0f );
+const vec4 GRUVBOX_FADED_PURPLE			= HEX_TO_RGBA( 0x8f3f71, 1.0f );
+const vec4 GRUVBOX_FADED_AQUA			= HEX_TO_RGBA( 0x427b58, 1.0f );
+const vec4 GRUVBOX_FADED_ORANGE			= HEX_TO_RGBA( 0xaf3a03, 1.0f );
---
+++ lodge-lib/gruvbox.h
@@ -0,0 +1,30 @@
+#ifndef _GRUVBOX_H
+#define _GRUVBOX_H
+
+#include "math4.h"
+
+extern const vec4 GRUVBOX_BRIGHT_RED;
+extern const vec4 GRUVBOX_BRIGHT_GREEN;
+extern const vec4 GRUVBOX_BRIGHT_YELLOW;
+extern const vec4 GRUVBOX_BRIGHT_BLUE;
+extern const vec4 GRUVBOX_BRIGHT_PURPLE;
+extern const vec4 GRUVBOX_BRIGHT_AQUA;
+extern const vec4 GRUVBOX_BRIGHT_ORANGE;
+
+extern const vec4 GRUVBOX_NEUTRAL_RED;
+extern const vec4 GRUVBOX_NEUTRAL_GREEN;
+extern const vec4 GRUVBOX_NEUTRAL_YELLOW;
+extern const vec4 GRUVBOX_NEUTRAL_BLUE;
+extern const vec4 GRUVBOX_NEUTRAL_PURPLE;
+extern const vec4 GRUVBOX_NEUTRAL_AQUA;
+extern const vec4 GRUVBOX_NEUTRAL_ORANGE;
+
+extern const vec4 GRUVBOX_FADED_RED;
+extern const vec4 GRUVBOX_FADED_GREEN;
+extern const vec4 GRUVBOX_FADED_YELLOW;
+extern const vec4 GRUVBOX_FADED_BLUE;
+extern const vec4 GRUVBOX_FADED_PURPLE;
+extern const vec4 GRUVBOX_FADED_AQUA;
+extern const vec4 GRUVBOX_FADED_ORANGE;
+
+#endif
