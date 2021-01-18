f9860a96cb537988206db20a07def42c291d5bec
Tim Sjöstrand <tim.sjostrand@gmail.com>
2021-01-18 15:42:30 +0100

    lodge-lib: dynbuf: initial commit

---
+++ lodge-lib/dynbuf.c
@@ -0,0 +1,91 @@
+#include "dynbuf.h"
+
+#include "membuf.h"
+
+#include "lodge_assert.h"
+
+#include <string.h>
+#include <stdlib.h>
+
+#define DYNBUF_CAPACITY_MIN 8
+
+static membuf_t dynbuf_to_membuf(dynbuf_t dynbuf)
+{
+	return membuf_make(*dynbuf.ptr, dynbuf.type_size * (*dynbuf.capacity), dynbuf.type_size);
+}
+
+static void dynbuf_maybe_grow(dynbuf_t dst, size_t new_count)
+{
+	ASSERT(dst.ptr);
+
+	if(!*dst.ptr || (new_count >= *dst.capacity)) {
+		size_t new_capacity = max(*dst.capacity, DYNBUF_CAPACITY_MIN);
+
+		if(new_count >= new_capacity) {
+			new_capacity <<= 1;
+		}
+
+		char *new_ptr = NULL;
+
+		if(*dst.ptr) {
+			new_ptr = realloc(*dst.ptr, new_capacity * dst.type_size);
+		} else {
+			new_ptr = malloc(new_capacity * dst.type_size);
+		}
+
+		ASSERT(new_ptr);
+
+		if(new_ptr) {
+			*dst.ptr = new_ptr;
+			*dst.capacity = new_capacity;
+		}
+	}
+}
+
+void dynbuf_new_inplace(dynbuf_t dynbuf, size_t default_capacity)
+{
+	//
+	// If this assert hits, you probably did not use `dynbuf_wrap()`.
+	//
+	ASSERT(dynbuf.type_size > 0);
+	*dynbuf.capacity = max(default_capacity, DYNBUF_CAPACITY_MIN);
+	*dynbuf.count = 0;
+	*dynbuf.ptr = NULL;
+}
+
+void dynbuf_free_inplace(dynbuf_t dynbuf)
+{
+	if(dynbuf.ptr) {
+		free(*dynbuf.ptr);
+		*dynbuf.ptr = NULL;
+	}
+}
+
+void dynbuf_clear(dynbuf_t dynbuf)
+{
+	*(dynbuf.count) = 0;
+}
+
+void* dynbuf_append(dynbuf_t dst, const void *src, size_t src_size)
+{
+	dynbuf_maybe_grow(dst, *dst.count + 1);
+	return membuf_append(dynbuf_to_membuf(dst), dst.count, src, src_size);
+}
+
+void* dynbuf_append_no_init(dynbuf_t dst)
+{
+	dynbuf_maybe_grow(dst, *dst.count + 1);
+	return membuf_append_no_init(dynbuf_to_membuf(dst), dst.count);
+}
+
+void* dynbuf_append_range(dynbuf_t dst, void *src, size_t src_type_size, size_t src_count)
+{
+	if(!src) {
+		return NULL;
+	}
+	ASSERT_OR(src_type_size == dst.type_size) {
+		return NULL;
+	}
+	dynbuf_maybe_grow(dst, *dst.count + src_count);
+	return membuf_append_range(dynbuf_to_membuf(dst), dst.count, src, src_type_size, src_count);
+}
---
+++ lodge-lib/dynbuf.h
@@ -0,0 +1,40 @@
+#ifndef _DYNBUF_H
+#define _DYNBUF_H
+
+#include <stddef.h>
+
+typedef struct dynbuf
+{
+	size_t		*capacity;
+	size_t		*count;
+	size_t		type_size;
+	char		**ptr;
+} dynbuf_t;
+
+
+//
+//	Can be used as on a struct that implements the dynbuf "interface":
+//
+//		struct my_buf
+//		{
+//			size_t			count;
+//			size_t			capacity;
+//			struct my_elem	*elements;
+//		} my_buf_global;
+//
+//		struct my_elem foo = { 0 };
+//		dynbuf_append(dynbuf_wrap(&my_buf_global), &foo, sizeof(foo));
+//	
+#define			dynbuf_wrap(DYNBUF) (dynbuf_t) { .capacity = &DYNBUF->capacity, .count = &DYNBUF->count, .type_size = sizeof(*DYNBUF->elements), .ptr = (char **)&DYNBUF->elements }
+#define			dynbuf_wrap_stack(DYNBUF_STACK) dynbuf_wrap( (&DYNBUF_STACK) )
+
+void			dynbuf_new_inplace(dynbuf_t dynbuf, size_t default_capacity);
+void			dynbuf_free_inplace(dynbuf_t dynbuf);
+
+void			dynbuf_clear(dynbuf_t dynbuf);
+
+void*			dynbuf_append(dynbuf_t dst, const void *src, size_t src_size);
+void*			dynbuf_append_no_init(dynbuf_t dst);
+void*			dynbuf_append_range(dynbuf_t dst, void *src, size_t src_type_size, size_t src_count);
+
+#endif
