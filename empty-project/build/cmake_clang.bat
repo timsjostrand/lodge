151747f3c1b18a09c09a3d4a13d869b7d8f856ad
Tim Sjöstrand <tim.sjostrand@gmail.com>
2021-10-17 23:46:04 +0200

    empty-project: removed

--- empty-project/build/cmake_clang.bat
+++
@@ -1,9 +0,0 @@
-if not defined BUILD_TYPE set BUILD_TYPE="Debug"
-mkdir clang
-cd clang
-mkdir "%BUILD_TYPE%"
-cd "%BUILD_TYPE%"
-cmake -DCMAKE_CC_COMPILER=clang-cl -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ../../../
-make run
-cd ../../
-pause
--- empty-project/build/cmake_vs10.bat
+++
@@ -1,3 +0,0 @@
-mkdir vs10
-cd vs10
-cmake -G "Visual Studio 10 2010" ../../
