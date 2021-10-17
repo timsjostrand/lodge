fe0362ce2176918bf90bffc9dfa66cf4af8dd527
Tim Sjöstrand <tim.sjostrand@gmail.com>
2021-10-17 23:45:12 +0200

    cmake: remove unused includes and helpers

--- cmake/generate-project.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set batdir=%~dp0
-echo NOTE: Project will be created in %CD%\GAME_NAME
-echo.
-set /p GAME_NAME="Enter project name: "
-cmake -DGAME_NAME="%GAME_NAME%" -P %batdir%\generate-project.cmake
-cd "%GAME_NAME%"
-echo Please update cmake_user.txt manually
-pause
